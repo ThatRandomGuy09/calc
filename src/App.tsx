4e39a50bed9b370da56b8dadb6647444fec49509
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-29 20:07:55 +0530

    Init and Added Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-10">
+      <h1 className="text-4xl font-bold">Tailwind CSS is working!</h1>
+    </div>
+  );
+}
+
+export default App;
